2772eccc175beae5f0391d441e3f78e6904e039a
Pedro Belli <pedro.belli2@gmail.com>
2015-12-01 20:20:31 -0200

    Mudanças em classes e criação da classe de Tipo de Cliente. Criação de controllers: Atendente, Gerente, Nutricionista e Tipo de Cliente.

--- src/java/models/Atendente.java
+++ src/java/models/Atendente.java
@@ -27,68 +27,136 @@ public class Atendente implements Serializable {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getNome() {
         return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public String getCpf() {
         return cpf;
     }
 
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
     public String getEmail() {
         return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getSenha() {
         return senha;
     }
 
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
     public int getTipoUsuario() {
         return tipoUsuario;
+    }
+
+    public void setTipoUsuario(int tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
     }
 
     public String getZipCode() {
         return zipCode;
     }
 
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
     public int getEstado() {
         return estado;
+    }
+
+    public void setEstado(int estado) {
+        this.estado = estado;
     }
 
     public String getCidade() {
         return cidade;
     }
 
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
     public String getBairro() {
         return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
     }
 
     public String getRua() {
         return rua;
     }
 
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
     public String getNumeroEndereco() {
         return numeroEndereco;
+    }
+
+    public void setNumeroEndereco(String numeroEndereco) {
+        this.numeroEndereco = numeroEndereco;
     }
 
     public String getComplemento() {
         return complemento;
     }
 
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
     public int getCodigoAreaTelefone() {
         return codigoAreaTelefone;
+    }
+
+    public void setCodigoAreaTelefone(int codigoAreaTelefone) {
+        this.codigoAreaTelefone = codigoAreaTelefone;
     }
 
     public String getNumeroTelefone() {
         return numeroTelefone;
     }
 
+    public void setNumeroTelefone(String numeroTelefone) {
+        this.numeroTelefone = numeroTelefone;
+    }
+
     public int getCodigoAreaCelular() {
         return codigoAreaCelular;
     }
 
+    public void setCodigoAreaCelular(int codigoAreaCelular) {
+        this.codigoAreaCelular = codigoAreaCelular;
+    }
+
     public String getNumeroCelular() {
         return numeroCelular;
+    }
+
+    public void setNumeroCelular(String numeroCelular) {
+        this.numeroCelular = numeroCelular;
     }
             
 }
---
+++ src/java/models/TipoCliente.java
@@ -0,0 +1,9 @@
+package models;
+
+import java.io.Serializable;
+
+public class TipoCliente implements Serializable {
+    private int    id;
+    private String nome;
+    private float  valorRefeicao;
+}
