7b7ceca4feecb8207213031b1af9f84f97e33a08
Layla Codogno <layla.codogno@outlook.com>
2015-12-07 21:50:36 -0200

    mask

--- web/assets/javascripts/app.js
+++ web/assets/javascripts/app.js
@@ -4,25 +4,25 @@ $(document).ready(function(){
     $(".monthpicker").inputmask("mm/yyyy");
     $(".time").inputmask("hh:mm");
     $('.area-code').inputmask('99');
-    $('.zip-code').inputmask('99.999-999');
+    $('.zip-code').inputmask('99.999-999', {removeMaskOnSubmit: true});
     $('.number').inputmask({alias: 'numeric', greedy : false, rightAlign: false});
     $('.percentage').inputmask({alias: 'percentage', greedy : false, rightAlign: false});
-    $('.federal-id').inputmask('999.999.999-99');
+    $('.federal-id').inputmask('999.999.999-99', {removeMaskOnSubmit: true});
     $('.cnpj').inputmask('99.999.999/9999-99');
-    $('.cellphones').inputmask('[9]9999-9999', {greedy : false, numericInput: true});
+    $('.cellphones').inputmask('(99) [9]9999-9999', {greedy : false, numericInput: true, removeMaskOnSubmit: true});
     $('.agency').inputmask('9999');
     $('.account').inputmask('99999[99999]');
 
 
-  Inputmask.extendAliases({
-    "currency": {
-      prefix: "R$ ",
-      placeHolder: '0,0',
-      autoUnmask: true,
-      rightAlign: false
-    }
-  });
-   $('.currency').inputmask({alias: 'currency'});
+    Inputmask.extendAliases({
+      "currency": {
+        prefix: "R$ ",
+        placeHolder: '0,0',
+        autoUnmask: true,
+        rightAlign: false
+      }
+    });
+    $('.currency').inputmask({alias: 'currency'});
 
     $('.email').inputmask({
       mask: "*{1,20}[.*{1,20}][.*{1,20}][.*{1,20}]@*{1,20}[.*{2,6}][.*{1,2}]",
